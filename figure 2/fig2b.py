a7c9022454beeaabdc3be057c9ad6d6b6b27d3e0
JialuZhang01 <202431250015@mail.bnu.edu.cn>
2026-04-04 15:49:31 +0800

    Update fig2b.py

--- figure 2/fig2b.py
+++ figure 2/fig2b.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Apr 15 11:22:53 2025
 
-@author: Administor
+@author: JialuZhang01
 """
 
 import os
@@ -15,44 +15,45 @@ import networkx as nx
 from matplotlib.colors import ListedColormap
 
 
-def mkdirectory(path):     # 若文件夹不存在，建立它
+def mkdirectory(path):    
+    """
+    Create a directory at the specified path if it does not already exist.
+    """
     if not os.path.exists(path):
         os.makedirs(path, exist_ok=True)
  
        
 if __name__ == "__main__":
-    # 网络涉及到的参数 
-    L_side = 1000
-    networkpath = 'network/'   # 网络文件夹   
-    # zeta取值的列表:[3,4,5,...19,20,30,40,50,...,90,100,500,1000]
-    zeta = 20  # 1000-243
-    source_p = 0.236
-    avg_k = 10   # 平均度
+    L_side = 1000   # Lattice length
+    networkpath = 'network/'   # the network's path.  
+    zeta = 20 
+    source_p = 0.236  # pc2
+    avg_k = 10   # Average degree
     i = 0
-    T_active = 6  # 邻居中有>=2个处于active时,inactive → active 
+    T_active = 6  # The activation threshold (T)
     sim_time = 1
     deal_resultpath = 'deal_result/'
-    deal_results_spatial =  deal_resultpath +'spatial_图2.d/'+ f'NetID{i}_avgk{avg_k}_zeta{zeta}'+'/'+f'source{source_p}_T{T_active}'+ '/spatial_result_total/'
+    deal_results_spatial =  deal_resultpath +'spatial_fig2.b/'+ f'NetID{i}_avgk{avg_k}_zeta{zeta}'+'/'+f'source{source_p}_T{T_active}'+ '/spatial_result_total/'
     
-  
-    f1 = open('deal_result/3、12、1000级联过程txt结果/'+'T='+str(T_active) +'_avgk='+str(avg_k) +'_t_S(gc)_nodes_zeta'+str(zeta)+'_'+str(source_p)+'.txt','r', encoding='utf-8-sig')
-    # 每一行：r* 节点1;节点2;..节点n(t0时位于最大连通分量中的节点); t1时位于最大连通分量中的节点 ...
+    # Define a custom colormap for the phase state: 
+    #  Green (#5BAB54) for LCC nodes,Purple (#885F9C) for others.
+    f1 = open('deal_result/3、12、1000_cascade_dynamic/'+'T='+str(T_active) +'_avgk='+str(avg_k) +'_t_S(gc)_nodes_zeta'+str(zeta)+'_'+str(source_p)+'.txt','r', encoding='utf-8-sig')
     t = 0
     for line_f1 in f1:
         line_f1 = line_f1.strip().split('\t')
         if line_f1[0] == 'p'+str(source_p):
-            for i in line_f1[1:]:  # i=[t0时位于最大连通分量中的节点,t1时位于最大连通分量中的节点,...]
-                color_matrix = np.zeros((L_side, L_side))  # 最初每个节点的矩阵元素都是0
-                lcc_nodes =[] # 当前时步最大连通分量中的节点集合
-                if i != 'null':  # 若当前时步位于最大连通分量中的节点列表不为空
-                    i = i.strip().split(';')  # t*时位于最大连通分量中的节点i=[节点1,节点2,...,;]
-                    for n1 in i[0:-1]:   # 因为最后一个元素是分号
+            for i in line_f1[1:]: # i = [nodes in the LCC at time t0, nodes in the LCC at time t1, ...]
+                color_matrix = np.zeros((L_side, L_side)) 
+                lcc_nodes =[] 
+                if i != 'null':  
+                    i = i.strip().split(';')  
+                    for n1 in i[0:-1]:  
                         lcc_nodes.append(ast.literal_eval(n1))   
-                for i0, j0 in lcc_nodes:  # 设置当前时步最大连通分量中的节点的矩阵元素为1
+                for i0, j0 in lcc_nodes:  
                     # color_matrix[int(i0-300)%L_side, int(j0+200)%L_side] = 1   # 12
                     color_matrix[int(i0), int(j0)] = 1
                     
-                cmap = ListedColormap(['#885F9C', '#5BAB54'])  # 蓝色、绿色
+                cmap = ListedColormap(['#885F9C', '#5BAB54'])  
                 plt.imshow(color_matrix, cmap=cmap, origin='lower', vmin=0,vmax=1)
                 plt.xticks([])
                 plt.yticks([])
@@ -63,7 +64,6 @@ if __name__ == "__main__":
                 
                 mkdirectory(deal_results_spatial+ str(0) + '_simulation/')  
                 plt.savefig(deal_results_spatial+ str(0) + '_simulation/'+ 'step_'+ str(t)+'.pdf',bbox_inches = 'tight')
-                # plt.savefig(deal_results_spatial+ str(0) + '_simulation/'+ 'step_'+ str(t)+'.png',bbox_inches = 'tight',dpi = 1000)
                 plt.close()
                 t+=1   
                
